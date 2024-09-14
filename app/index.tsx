cd9c85e162e1ddd57a57219f69cec852dec0e72b
Reinboo <nivalis@protonmail.com>
2024-09-14 20:48:11 +0200

    Style the Welcome page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,53 @@
+import { StyleSheet } from "react-native";
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
-import { Link } from "expo-router";
-import { Routes } from "@/constants/Routes";
+import { ThemedButton } from "@/components/ThemedButton";
+import { Colors } from "@/constants/Colors";
 
-const WelcomePage = () => {
+export default function WelcomePage() {
   return (
-    <ThemedView>
-      <ThemedText>WELCOME</ThemedText>
-      <Link href={Routes.signIn}>Sign in</Link>
+    <ThemedView style={styles.container}>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText style={styles.titleText}>Welcome</ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.buttonsContainer}>
+        <ThemedButton text="Sign in" />
+        <ThemedButton text="Create an account" type="subtitle" />
+      </ThemedView>
     </ThemedView>
   );
-};
+}
 
-export default WelcomePage;
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    width: "100%",
+    padding: 16,
+  },
+  titleContainer: {
+    flex: 3,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    width: "100%",
+  },
+  titleText: {
+    color: Colors.light.text,
+    fontSize: 48,
+    lineHeight: 52,
+    fontWeight: "bold",
+  },
+  buttonsContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    width: "100%",
+    flex: 1,
+    gap: 5,
+  },
+});
