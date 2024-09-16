2257498d68193fc920fd496dee360d63ccdb6d6e
Reinboo <nivalis@protonmail.com>
2024-09-16 13:37:25 +0200

    Create strings file

---
+++ constants/strings.ts
@@ -0,0 +1,24 @@
+export const t = {
+  en: {
+    translation: {
+      title: {
+        welcome: "Welcome",
+        welcomeBack: "Welcome Back",
+        createAccount: "Create an Account",
+        profile: "Profile",
+        editProfile: "Edit profile",
+        changePassword: "Change password",
+      },
+      button: {
+        signIn: "Sign in",
+        createAccount: "Create an account",
+        create: "Create",
+      },
+      label: {
+        name: "Name",
+        email: "E-Mail",
+        password: "Password",
+      },
+    },
+  },
+};
