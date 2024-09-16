6a2e9c525c330934ad00b3725ce91c14abccbaf1
Reinboo <nivalis@protonmail.com>
2024-09-16 14:12:42 +0200

    Fix profile update after edit

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -9,56 +9,46 @@ import { router } from "expo-router";
 import { Routes } from "@/constants/Routes";
 
 import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
-import * as LocalAuthentication from "expo-local-authentication";
 
 interface UserContextType {
   user: FirebaseAuthTypes.User | null;
   isAuthenticated: boolean;
-  isAuthorized: boolean;
 }
 
 const UserContext = createContext<UserContextType | null>(null);
 
 export function UserProvider({ children }: { children: ReactNode }) {
-  // TODO: Biometrics
   const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-  const [isAuthorized, setIsAuthorized] = useState<boolean>(false);
 
+  // Handle user being logged in/out
   useEffect(() => {
     const unsubscribe = auth().onAuthStateChanged(async (user) => {
       if (user !== null) {
         setUser(user);
         setIsAuthenticated(true);
 
-        if (isAuthorized) router.push(Routes.profile); // TODO: Change to calendar
-
-        try {
-          // TODO: Check hardware
-          const result = await LocalAuthentication.authenticateAsync({
-            promptMessage: "Authenticate w biometrics",
-          });
-
-          if (result.success) {
-            setIsAuthorized(true);
-            router.push(Routes.profile); // TODO: Change to calendar
-          } else {
-            // TODO: Error
-            setIsAuthorized(false);
-          }
-        } catch (error: any) {}
+        router.replace(Routes.profile); // TODO: Change to calendar
       } else {
         setUser(null);
         setIsAuthenticated(false);
-        router.push(Routes.welcome);
+        router.replace(Routes.welcome);
       }
     });
 
     return unsubscribe;
   }, []);
 
+  // Handle profile edits
+  useEffect(() => {
+    const unsubscribe = auth().onUserChanged((user) => {
+      setUser(user);
+    });
+    return unsubscribe;
+  }, []);
+
   return (
-    <UserContext.Provider value={{ user, isAuthenticated, isAuthorized }}>
+    <UserContext.Provider value={{ user, isAuthenticated }}>
       {children}
     </UserContext.Provider>
   );
