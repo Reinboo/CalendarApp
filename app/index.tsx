4b67160e12d5f83ae6b9fef333b96171c518d34f
Reinboo <nivalis@protonmail.com>
2024-09-16 14:06:33 +0200

    Implement authentication within the welcome page

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,63 @@
-import { StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet } from "react-native";
+import { SheetManager } from "react-native-actions-sheet";
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedButton } from "@/components/ThemedButton";
 import { Colors } from "@/constants/Colors";
+import { useLayoutEffect } from "react";
+import auth from "@react-native-firebase/auth";
+import { router, useRootNavigationState } from "expo-router";
+import { Routes } from "@/constants/Routes";
+
+const WelcomeBackground = require("@/assets/images/background.png");
 
 export default function WelcomePage() {
+  const rootNavigationState = useRootNavigationState();
+
+  useLayoutEffect(() => {
+    const user = auth().currentUser;
+
+    if (
+      user !== null &&
+      rootNavigationState?.routeNames?.includes(Routes.profile.substring(1))
+    ) {
+      router.replace(Routes.profile);
+    }
+  }, [rootNavigationState]);
+
+  const handlePressSignInSheet = async () => {
+    SheetManager.show("authentication", { payload: { isRegistering: false } });
+  };
+
+  const handlePressRegisterSheet = () => {
+    SheetManager.show("authentication", { payload: { isRegistering: true } });
+  };
+
   return (
-    <ThemedView style={styles.container}>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText style={styles.titleText}>Welcome</ThemedText>
+    <ImageBackground
+      source={WelcomeBackground}
+      resizeMode="cover"
+      style={styles.background}
+    >
+      <ThemedView style={styles.container}>
+        <ThemedView style={styles.titleContainer}>
+          <ThemedText style={styles.titleText}>Welcome</ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.buttonsContainer}>
+          <ThemedButton text="Sign in" onPress={handlePressSignInSheet} />
+          <ThemedButton
+            text="Create an account"
+            type="subtitle"
+            onPress={handlePressRegisterSheet}
+          />
+        </ThemedView>
       </ThemedView>
-      <ThemedView style={styles.buttonsContainer}>
-        <ThemedButton text="Sign in" />
-        <ThemedButton text="Create an account" type="subtitle" />
-      </ThemedView>
-    </ThemedView>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {},
   container: {
     display: "flex",
     justifyContent: "center",
@@ -34,12 +73,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
     height: "100%",
     width: "100%",
+    marginTop: 160,
   },
   titleText: {
     color: Colors.light.text,
     fontSize: 48,
     lineHeight: 52,
-    fontWeight: "bold",
+    fontFamily: "InterBold",
   },
   buttonsContainer: {
     display: "flex",
