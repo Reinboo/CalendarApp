60eec1e586704406b0693a7b81ef2cea0e506bb4
Reinboo <nivalis@protonmail.com>
2024-09-21 17:50:32 +0200

    Adjust ThemedDateTimeField component

--- components/ThemedDateTimeField.tsx
+++ components/ThemedDateTimeField.tsx
@@ -1,14 +1,14 @@
-import { Ionicons } from "@expo/vector-icons";
 import { StyleSheet, ViewStyle } from "react-native";
 import { Colors } from "@/constants/Colors";
-import { ThemedTextInput } from "./ThemedTextInput";
 import { ThemedView } from "./ThemedView";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import { useState } from "react";
+import { IconButton, Text, TouchableRipple } from "react-native-paper";
 
 interface ThemedDateTimeFieldProps {
   onChange: (date: Date) => void;
   label: string;
+  mode?: "date" | "time";
   value: Date;
   containerStyle?: ViewStyle;
 }
@@ -16,43 +16,67 @@ interface ThemedDateTimeFieldProps {
 export default function ThemedDateTimeField({
   onChange,
   value,
+  mode = "date",
   label,
   containerStyle,
 }: ThemedDateTimeFieldProps) {
   const [isDatePickerOpen, setIsDatePickerOpen] = useState<boolean>(false);
-  const [date, setDate] = useState<Date>(new Date());
+  const [date, setDate] = useState<Date>(value || new Date());
 
   const handleConfirmDate = (date: Date) => {
     onChange(date);
+    setDate(date);
     setIsDatePickerOpen(false);
   };
 
   const handleCloseDatePicker = () => setIsDatePickerOpen(false);
 
-  const formattedDate = " ";
+  const dateFormat: Intl.DateTimeFormatOptions = {
+    month: "long",
+    day: "numeric",
+    weekday: "long",
+  };
+  const timeFormat: Intl.DateTimeFormatOptions = {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  };
+  const formattedDate =
+    mode === "date"
+      ? date.toLocaleDateString("en-GB", dateFormat)
+      : date.toLocaleTimeString("en-GB", timeFormat);
 
   return (
-    <ThemedView style={[styles.infoContainer, containerStyle]}>
-      <ThemedView style={styles.iconContainer}>
-        <Ionicons
-          size={36}
-          name={"calendar-outline"}
-          color={Colors.light.highlight}
+    <TouchableRipple
+      rippleColor={Colors.light.highlight}
+      onPress={() => setIsDatePickerOpen(true)}
+      style={{ borderRadius: 16 }}
+    >
+      <ThemedView style={[styles.infoContainer, containerStyle]}>
+        <ThemedView style={styles.iconContainer}>
+          <IconButton
+            size={32}
+            icon={mode === "date" ? "calendar-edit" : "clock-edit-outline"}
+            iconColor={Colors.light.highlight}
+          />
+        </ThemedView>
+        <ThemedView style={styles.valueContainer}>
+          <Text style={styles.label}>{label}</Text>
+          <Text style={styles.value}>
+            {formattedDate || `Press to enter ${mode}`}
+          </Text>
+        </ThemedView>
+        <DateTimePickerModal
+          isVisible={isDatePickerOpen}
+          mode={mode}
+          onConfirm={handleConfirmDate}
+          onCancel={handleCloseDatePicker}
+          accentColor={Colors.light.text}
+          textColor={Colors.light.text}
+          date={date}
         />
       </ThemedView>
-      <ThemedTextInput
-        editable={false}
-        label={label}
-        value={formattedDate}
-        style={{ flexGrow: 1 }}
-      />
-      <DateTimePickerModal
-        isVisible={isDatePickerOpen}
-        mode="datetime"
-        onConfirm={handleConfirmDate}
-        onCancel={handleCloseDatePicker}
-      />
-    </ThemedView>
+    </TouchableRipple>
   );
 }
 
@@ -61,10 +85,25 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     flexDirection: "row",
     gap: 16,
+    borderRadius: 16,
   },
   iconContainer: {
     justifyContent: "center",
     alignItems: "center",
     width: 40,
+    height: 70,
+  },
+  valueContainer: {
+    justifyContent: "center",
+    gap: 5,
+  },
+  label: {
+    color: "lightgrey",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  value: {
+    fontSize: 18,
+    color: Colors.light.text,
   },
 });
